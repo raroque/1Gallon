49ac4b16327651739571e4e468554c7583c54c78
Christian Raroque <Chris@Christians-MacBook-Pro-2.local>
2016-11-10 16:51:47 -0600

    Initial Commit

---
+++ 1 Gallon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1 Gallon
+//
+//  Created by Christian Raroque on 11/10/16.
+//  Copyright © 2016 AloaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
