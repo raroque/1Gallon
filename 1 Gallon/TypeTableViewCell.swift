dcacd1617e645c9b2a1e57e392ccbd143e881eff
Christian Raroque <Chris@christiansmbp2.attlocal.net>
2016-12-29 02:00:23 -0600

    first commit

---
+++ 1 Gallon/TypeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TypeTableViewCell.swift
+//  1 Gallon
+//
+//  Created by Christian Raroque on 11/26/16.
+//  Copyright © 2016 AloaLabs. All rights reserved.
+//
+
+import UIKit
+
+class TypeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
